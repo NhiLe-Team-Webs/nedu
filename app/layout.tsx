4e65dd0e6e2d57af86ecb861bdfab6f0a8f48cd0
Vũ Hoàng Khang <160208535+khangvhdevs@users.noreply.github.com>
2025-11-24 19:37:03 +0700

    feat(data): add plugin Google Analytics to track data

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,18 @@ export default function RootLayout({
     <html lang="vi">
       <head>
         <meta charSet="utf-8" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BK6LMDZEH4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BK6LMDZEH4');
+          `}
+        </Script>
       </head>
       <body>
         <CartProvider>
